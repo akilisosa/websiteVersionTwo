245b89fdfeea2bae854bb7bf62002e521a614384
akilisosa <akilsosa@gmail.com>
2019-01-14 17:58:05 -0700

    first commit

---
+++ js/buildWeb.js
@@ -0,0 +1,17 @@
+function buildWeb(onlick){
+
+    let newArr = [ {webX : 100, webY:100, webDX:0, webDY:0, searchimage:null } , 
+                   {webX:100,webY:100, webDX: 100, webDY: 0,searchimage:null},
+                   {webX:100,webY:100, webDX: 0, webDY: 100, searchimage:null},
+                   {webX:100,webY:100, webDX: 100, webDY: 100, searchimage:null}];
+
+    function firstClick(){
+
+       
+
+    }
+return newArr;
+
+console.log(newArr);
+
+}
